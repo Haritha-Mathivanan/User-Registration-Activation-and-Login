52acae4310a6428d8e858cebdbb4823231b6a7d6
Haritha-Mathivanan <81553150+Haritha-Mathivanan@users.noreply.github.com>
2021-11-22 13:39:54 +0530

    Add files via upload

---
+++ Account Creation/AccountCreation/Accountactivation.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Net.Mail;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+
+namespace AccountCreation
+{
+    public class Accountactivation
+    {
+        Dictionary<string, UserDetails> Register;
+        Dictionary<string, UserDetails> Activated;
+        public Accountactivation() 
+        {
+           Register = FileRead("D:\\Root\\DataStorageFiles\\Register.txt");
+           //Activated = FileRead("D:\\Root\\DataStorageFiles\\Activated.txt");
+        }
+        public void AddDetails(string Name, int Age, string Dob, string Username, string Password, string Email, string Address)
+        {
+           
+            try
+            {
+                if (IsSymbol(Username) == true)
+                {
+                    //throw new InvalidDataException("Username should not contains Symbols ");
+                   // throw new ArgumentException("Username should not contains Symbols ");
+                    throw new FormatException("Username should not contains Symbols ");
+                }
+               
+                if(IsValid(Password))
+                {
+                    Password=ChangePasswordToHash(Password);
+                   
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid Argument,Password Should Contains Symbols,numbers,Upper case and Lowercase letter ");
+                }
+            }
+            catch(FormatException dex)
+            {
+                throw new FormatException(dex.Message);
+            }
+            catch(ArgumentException ae)
+            {
+               throw new ArgumentException(ae.Message);
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            UserDetails userdetail = new UserDetails(Name, Age, Dob, Password, Email, Address);
+            Register.Add(Username,userdetail);
+            //Password=ChangePasswordToHash(Password); 
+        }
+        private bool IsValid(string pass)
+        {
+            int weight = 0;
+            if (pass.Length > 7)
+            {
+                weight += 1;
+            }
+            if (IsSymbol(pass))
+            {
+                weight += 1;
+            }
+            if (IsDigit(pass))
+            {
+                weight += 1;
+
+            }
+            if (IsUpper(pass))
+            {
+                weight += 1;
+
+            }
+            if (IsLower(pass))
+            {
+                weight += 1;
+            }
+
+            if (weight == 5)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private bool IsSymbol(string str)
+        {
+            char[] symbol = new char[] { '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '>', '<', '?', '/', ':', ';', '=', '"', '[', '{', '}', ']', '+', '-', '_', '|' };
+            for (int i = 0; i < str.Length; i++)
+            {
+                for (int j = 0; j < symbol.Length; j++)
+                {
+                    if (str[i] == symbol[j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+
+        }
+        private bool IsDigit(string str)
+        {
+            char[] digit = new char[] { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+            int[] dig = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+            for (int i = 0; i < str.Length; i++)
+            {
+                for (int j = 0; j < dig.Length; j++)
+                {
+                    if (str[i] == digit[j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        private bool IsUpper(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] >= 65 && str[i] <= 91)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private bool IsLower(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] >= 92 && 118 >= str[i])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public Dictionary<string, UserDetails> FileRead(string path)
+        {
+            Dictionary<string, UserDetails> Registerdict = new Dictionary<string, UserDetails>(); 
+            StreamReader read = new StreamReader(path);
+            string line ;
+
+            while ((line = read.ReadLine()) != null)
+            {
+                string[] details = line.Split('|');
+                int i = 0;
+
+                string name = details[i]; i++;
+                int age = Convert.ToInt32(details[i]); i++;
+                string dob = details[i]; i++;
+                string username = details[i]; i++;
+                
+                string password = details[i]; i++;
+                string email = details[i]; i++;
+                string address = details[i];
+               // line = read.ReadLine();
+                UserDetails user = new UserDetails(name, age, dob, password, email, address);
+               // user.Username = username;
+                // usercheck user = new usercheck("jare",19,"12/09/2002","sfd123sfd","jksds@gmail.com","xxx");
+                Registerdict.Add(username, user);
+            }
+            read.Close();
+            return Registerdict;
+        }
+        public void FileWrite(string path)
+        {
+            StreamWriter write = new StreamWriter(path);
+            foreach (KeyValuePair<string, UserDetails> x in Register)
+            {
+                Console.WriteLine(x.Key + " " + x.Value);
+                string lin = x.Value.Name + "|";
+                lin += Convert.ToString(x.Value.Age) + "|" + x.Value.Dob + "|" + x.Key + "|" + x.Value.Password + "|" + x.Value.Email + "|" + x.Value.Address;
+                write.WriteLine(lin);
+            }
+            write.Close();
+
+        }
+        public int GenerateOTP()
+        {
+            Random ran = new Random();
+            return ran.Next(10000,100000);
+        }
+        public bool SendEmail(string receiver)
+        {
+            try
+            {
+                if (IsValidEmail(receiver))
+                {
+
+                    string OTP = Convert.ToString(GenerateOTP());
+                    string body = "Your OTP for Account Activation or Verification is " + OTP;
+                    MailMessage message = new MailMessage("Projectvalidation2021@gmail.com", receiver, OTP, body);
+                    SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+                    System.Net.NetworkCredential basicCredential1 = new System.Net.NetworkCredential("Projectvalidation2021@gmail.com", "Project2021");
+                    client.EnableSsl = true;
+                    client.UseDefaultCredentials = false;
+                    client.Credentials = basicCredential1;
+                    client.Send(message);
+                    return true;
+
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid Email Id");
+                }
+
+            }
+            catch(ArgumentException ae)
+            {
+                throw new ArgumentException(ae.Message);
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return false;
+           
+        }
+        public bool IsValidEmail(string emailaddress)
+        {
+            string expression = "\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*";
+            if (Regex.IsMatch(emailaddress, expression))
+            {
+                if (Regex.Replace(emailaddress, expression, string.Empty).Length == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        //public string Checkdictionary()
+        //{
+        //    string x = "";
+        //    foreach(KeyValuePair<string,UserDetails> c in Register)
+        //    {
+        //        x += c.Value.Name + " " + c.Value.Age + " " + c.Value.Dob + " " + c.Key + " " + c.Value.Password + "  " + c.Value.Email + " " + c.Value.Address;
+        //    }
+        //    return x;
+        //}
+        private string ChangePasswordToHash(string pass)
+        {
+            byte[] pa = ASCIIEncoding.ASCII.GetBytes(pass);
+            byte[] tmmp = new MD5CryptoServiceProvider().ComputeHash(pa);
+            string hashpass = ByteToString(tmmp);
+            return hashpass;
+
+        }
+        private string ByteToString(byte[] ar)
+        {
+            StringBuilder s = new StringBuilder();
+            for (int i = 0; i < ar.Length;i++ )
+            {
+                s.Append(Convert.ToString(ar[i]));
+            }
+            return s.ToString();
+        }
+        private bool IsHashSame(string dicpass,string inputpass)
+        {
+            bool IsEqual = false;
+            if(dicpass.Length==inputpass.Length)
+            {
+                int i = 0;
+                while((i<dicpass.Length)&&dicpass[i]==inputpass[i])
+                {
+                    i += 1;
+                }
+                if (i == dicpass.Length)
+                {
+                    IsEqual = true;
+                }
+            }
+            return IsEqual;
+        }
+        public bool Login(string username,string password)
+        {
+           string pass = ChangePasswordToHash(password);
+            foreach(KeyValuePair<string,UserDetails> x in Register)
+            {
+                if ( x.Key==username && IsHashSame(x.Value.Password,pass))
+                {
+                    
+                        return true;
+                    
+                }
+            }
+            //foreach(KeyValuePair<string,UserDetails> x in Activated)
+            //{
+            //    if (x.Key == username && x.Value.Password == pass)
+            //    {
+            //        return true;
+            //    }
+            //}
+            return false;
+
+        }
+         public bool PasswordReset(string password,string username)
+        {
+            string pass=ChangePasswordToHash(password);
+            foreach(KeyValuePair<string,UserDetails> x in Register)
+            {
+                if (x.Key == username)
+                {
+                    x.Value.Password = pass;
+                    return true;
+                }
+            }
+            foreach(KeyValuePair<string,UserDetails> x in Activated)
+            {
+                if(x.Key==username)
+                {
+                    x.Value.Password = pass;
+                    return true;
+                }
+            }
+            return false;
+        }
+       
+    }
+}
---
+++ Account Creation/AccountCreation/UserDetails.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+
+
+
+namespace AccountCreation
+{
+    public class UserDetails
+    {
+        string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set 
+            {
+                try
+                {
+                    if (value.Length >= 3 && value.Length <= 15)
+                    {
+                        _name = value;
+                    }
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException("Name Contains Only 3 to 15 Characters");
+                    }
+                }
+                catch (ArgumentOutOfRangeException argex)
+                {
+                    throw new ArgumentOutOfRangeException(argex.Message);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+        }
+        int _age;
+
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                try
+                {
+                    if (value >= 18 && value <= 60)
+                    {
+                        _age = value;
+                    }
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException("Invalid Age Limit");
+                    }
+                }
+                catch (ArgumentOutOfRangeException argex)
+                {
+                    throw new ArgumentOutOfRangeException(argex.Message+"/n Age Limit Should be 18 to 60");
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+        }
+        string _dob;
+
+        public string Dob
+        {
+            get { return _dob; }
+            set
+            {
+
+                try
+                {
+                    string today = DateTime.Now.ToString("d/MM/yyyy");
+                    int agedif = YearDifference(today, value);
+                    if (agedif >= 18 && agedif <= 60)
+                    {
+                        _dob = value;
+                    }
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException("Invalid Age ");
+                    }
+                }
+                catch (ArgumentOutOfRangeException ex)
+                {
+                    throw new ArgumentOutOfRangeException(ex.Message);
+                }
+                catch (Exception exp)
+                {
+                    throw new Exception(exp.Message);
+                }
+            }
+        }
+        //string _username;
+
+        //public string Username
+        //{
+        //    get { return _username; }
+        //    set 
+        //    {
+        //        try
+        //        {
+        //            if (IsSymbol(value)==false)
+        //            {
+        //                _username = value;
+        //            }
+        //            else
+        //            {
+        //                throw new ArgumentException("Username should not contains Symbols ");
+        //            }
+        //        }
+        //        catch(ArgumentException ae)
+        //        {
+        //            Console.WriteLine(ae.Message);
+        //        }
+        //        catch(Exception ex)
+        //        {
+        //            Console.WriteLine(ex.Message);
+        //        }
+                
+        //    }
+        //}
+        private bool IsSymbol(string str)
+        {
+            char[] symbol = new char[] { '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '>', '<', '?', '/', ':', ';', '=', '"', '[', '{', '}', ']', '+', '-', '_', '|' };
+            for (int i = 0; i < str.Length; i++)
+            {
+                for (int j = 0; j < symbol.Length; j++)
+                {
+                    if (str[i] == symbol[j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+
+        }
+        private int YearDifference(string today, string dateofbirth)
+        {
+
+            string[] td = today.Split('-');
+            string[] bd = dateofbirth.Split('/');
+            int date1 = Convert.ToInt32(td[0]);
+            int date2 = Convert.ToInt32(bd[0]);
+            int mon1 = Convert.ToInt32(td[1]);
+            int mon2 = Convert.ToInt32(bd[1]);
+            int year1 = Convert.ToInt32(td[2]);
+            int year2 = Convert.ToInt32(bd[2]);
+            int diff = year1 - year2;
+            if (mon2 > mon1)
+            {
+                diff = diff - 1;
+            }
+            else if (mon2 == mon1 && date2 < date1)
+            {
+                diff = diff - 1;
+            }
+            return diff;
+
+
+        }
+        private bool IsLeap(int year)
+        {
+            bool Isleap = false;
+            if (year % 4 == 0&&year%100==0&&year%400==0)
+            {
+                Isleap = true;
+            }
+            if (year % 100 != 0 && year % 4 == 0)
+            {
+                Isleap = true;
+            }
+            return Isleap;
+        }
+        string _password;
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+            }
+        }
+       
+        string _email;
+
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+               // _email = value;
+                try
+                {
+                    if (IsValidEmail(value))
+                    {
+                        _email = value;
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Invalid Email Id");
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                    throw new ArgumentException(ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+        }
+        private bool IsValidEmail(string emailaddress)
+        {
+            string expression = "\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*";
+            if (Regex.IsMatch(emailaddress, expression))
+            {
+                if (Regex.Replace(emailaddress, expression, string.Empty).Length == 0)
+                {
+                    return true;  
+                }
+            }
+            return false;
+        }
+        string _address;
+
+        public string Address
+        {
+            get { return _address; }
+            set 
+            {
+                try
+                {
+                    if (value.Length <= 120)
+                    {
+                        _address = value;
+                    }
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException("Address can Contains only below 120 characters");
+                    }
+                }
+                catch (ArgumentOutOfRangeException ex)
+                {
+                    throw new ArgumentOutOfRangeException(ex.Message);
+                }
+                catch(Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+        }
+        public UserDetails(string name,int age,string dob,string password,string email,string address)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Dob = dob;
+            this.Password = password;
+            this.Email = email;
+            this.Address = address;
+        }
+    }
+}
